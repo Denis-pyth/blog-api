40850a8193773845c62913cc6bbe841c8ff8d4aa
Denis-pyth <desirefeyisola2@gmail.com>
2026-03-13 06:03:33 -0700

    migrate: routes/auth.routes.ts

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,7 +1,6 @@
-import express from "express";
+import express, { Router } from "express";
 import { register, login } from "../controllers/auth.controller.js";
 
-const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
